26045e74c3130bd2a47faff2cb7f5d8c0e553424
Mandara <mandara.kudaliyanagedonperera@student.kdg.be>
2025-02-12 10:32:02 +0100

    ci

---
+++ src/services/firebase/firebase.ts
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDBCI-gWDTlrhUV8wPaI23ZML-wumwJTlc",
+    authDomain: "betterspy-50c8c.firebaseapp.com",
+    projectId: "betterspy-50c8c",
+    storageBucket: "betterspy-50c8c.firebasestorage.app",
+    messagingSenderId: "48226132790",
+    appId: "1:48226132790:web:9787bb721e8a6e7fce0997"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
