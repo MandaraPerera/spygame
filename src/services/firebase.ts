9716a8dc9881d1abee18993b9040d953024fa5a5
Mandara Perera <mandara.kudaliyanagedonperera@student.kdg.be>
2025-05-28 00:05:10 +0200

    Added Firebase App Check

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,7 @@
 import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
+import {initializeAppCheck, ReCaptchaEnterpriseProvider} from "firebase/app-check";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD6PrJ-zIsE8GmLtZ5Rks_4hYraYSltscY",
@@ -15,7 +16,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 // self.FIREBASE_APPCHECK_DEBUG_TOKEN = true
-// export const appCheck = initializeAppCheck(app, {
-//     provider: new ReCaptchaEnterpriseProvider(""),
-//     isTokenAutoRefreshEnabled: true,
-// })
+export const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaEnterpriseProvider("6LcwHEwrAAAAAN_6-qT1xHggsOrxDe9AtdaLfpAv"),
+    isTokenAutoRefreshEnabled: true,
+})
