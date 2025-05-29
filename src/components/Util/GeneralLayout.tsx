150e2276e0ea2fc068ee3cec986a89d7c5bb1c18
Mandara Perera <mandara.kudaliyanagedonperera@student.kdg.be>
2025-05-29 23:53:27 +0200

    Quick padding fix

--- src/components/Util/GeneralLayout.tsx
+++ src/components/Util/GeneralLayout.tsx
@@ -25,7 +25,7 @@ export function GeneralLayout({children}: GeneralLayoutProps) {
 
     return (
         <>
-            <VStack minH={`calc(var(--vh, 1vh) * 100)`} display="flex" alignItems="center" px={4} py={16}>
+            <VStack minH={`calc(var(--vh, 1vh) * 100)`} display="flex" alignItems="center" px={4} pt={8} pb={12}>
                 <VStack w="100%" flex={1}>
                     {children}
                 </VStack>
